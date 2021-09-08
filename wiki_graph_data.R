24ddba7375c1e2d0f049d707c003c370073ccfb1
Elis Carlberg Larsson <elica96@liu.se>
2021-09-08 16:03:42 +0200

    Description of the data created data.

---
+++ wiki_graph_data.R
@@ -0,0 +1,14 @@
+#'
+#' A data set containing a graph taken from the Wikipedia page of Dijkstra's algorithm.
+#'
+#' @format A data frame with 18 rows and 3 variables.
+#' @name wiki_graph
+#' \describe{
+#'   \item{v1}{Edges of the graph.}
+#'   \item{v2}{Edges of the graph.}
+#'   \item{w}{Weight of the edge.}
+#' }
+#' @docType data
+#' @keywords datasets
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
