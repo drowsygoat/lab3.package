97e7261526b4d642db80418803db8b2a942c8532
Lechu <32292322+drowsygoat@users.noreply.github.com>
2021-09-08 13:27:20 +0200

    some modifications

--- euclidean.R
+++ euclidean.R
@@ -1,9 +1,9 @@
 euclidean <- function(x, y) {
-  if (!(is.integer(x) && is.numeric(x)) | !(is.numeric(x) && length(x) != 1L)) {
+  if (!(is.integer(x) | is.numeric(x)) & length(x) != 1L) {
     stop("x is not a numeric integer or scalar.")
-  } else if (!(is.integer(y) && is.numeric(y)) | !(is.numeric(y) && length(y) != 1L)) {
-    stop("y is not a numeric integer or scalar.")
-  } else {
-    sqrt(sum((x - y) ^ 2))
   }
+  if (!(is.integer(y) | is.numeric(y)) | length(y) != 1L) {
+    stop("y is not a numeric integer or scalar.")
+  }
+  sqrt(sum((x - y) ^ 2)) # sth wrong here?
 }
