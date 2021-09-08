58a337acf0c5a45accc51322225a0dd92fef5af8
Elis Carlberg Larsson <elica96@liu.se>
2021-09-08 17:38:26 +0200

    Added folder to move scripts.

---
+++ R/lab3.R
@@ -0,0 +1,9 @@
+#' LAB 3 PACKAGE
+#' 
+#' Solutions to Lab 3 in Advanced R programming (73A94) at Linkoping University fall 2021. The package estimates Euclidean distance and implements Djikstra's algorithm to find the shortest path between two nodes. 
+#' @docType package
+#' @author Lech Kaczmarczyk \email{lech.kaczmarczyk@liu.se} 
+#' @author Elis Carlberg Larsson \email{elis.carlberg.larsson@liu.se}
+#' @name lab3.package
+NULL
+
